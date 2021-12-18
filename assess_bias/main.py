348edbe04a290c03407e2d0bccb9b574d76f4d9f
thearol <thearollesloth@hotmail.com>
2021-12-18 12:21:43 +0100

    plots

--- assess_bias/main.py
+++ assess_bias/main.py
@@ -1,7 +1,7 @@
 from utility_functions import weat_func
 from print_similarities import print_similarities
-from viz import plot_words, restrict_wv, tsne_plot
-from viz1 import equalize_visualization
+from viz import plot_words, restrict_wv, tsne_plot, equalize_visualization
+#from viz1 import equalize_visualization
 from gensim.models.keyedvectors import KeyedVectors
 from danlp.models.embeddings import load_wv_with_gensim
 import argparse
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     # parse args
     args = parser.parse_args()
     
+    # LOAD MODEL
     print("loading model")
     if args.embedding_filename.endswith(".wv"):
         model = load_wv_with_gensim(args.embedding_filename)
@@ -29,20 +30,21 @@ if __name__ == "__main__":
      
     print("loading debiased model")
     debiased_model = KeyedVectors.load_word2vec_format(os.path.join("..", "embeddings", f"{args.model_alias}_{args.debiased_filename}"), binary=True)
-     
+    
+    # WEAT 
+
     # define attribute words
     male = ['mandlig', 'mand','dreng','bror','han','ham','hans','søn']
     female = ['kvindelig', 'kvinde', 'pige', 'søster', 'hun', 'hende', 'hendes', 'datter'] 
 
     # define target words
-    #science = ['videnskab', 'teknologi', 'fysik', 'kemi', 'computer', 'eksperiment', 'data', 'biologi'] 
     arts = ['poesi', 'kunst', 'dans', 'litteratur', 'roman', 'symfoni', 'drama', 'skulptur'] 
     math = ['matematik', 'algebra', 'geometri', 'calculus', 'ligning', 'udregning', 'tal', 'addition'] 
     career = ['leder', 'ledelse', 'professionel', 'virksomhed', 'løn', 'kontor', 'forretning','karriere'] 
     family = ['hjem','forældre', 'børn', 'familie','bedsteforældre', 'ægteskab', 'bryllup', 'slægtninge'] 
     
-    #print("getting WEAT scores")
-    '''
+    print("getting WEAT scores")
+    
     # get WEAT scores model
     weat_func(model, f"biased_{args.model_alias}", "career", "family", 10000, male, female, career, family)
     weat_func(model, f"biased_{args.model_alias}", "math", "arts", 10000, male, female, math, arts)
@@ -51,6 +53,8 @@ if __name__ == "__main__":
     weat_func(debiased_model, f"debiased_{args.model_alias}", "career", "family", 10000, male, female, career, family)
     weat_func(debiased_model, f"debiased_{args.model_alias}", "math", "arts", 10000, male, female, math, arts)
     
+    # ASSESMENT PLOTS
+    '''
     # load professions
     professions_path = os.path.join("..", "data", "da_professions.json")
     with open(professions_path, "r") as f:
@@ -61,27 +65,27 @@ if __name__ == "__main__":
     with open(genderspecific_path, "r") as f:
         gender_specific_words = json.load(f)
     '''
-    # sample words 
+
+    # example words 
     profession_sample = ['sygeplejerske', 'sekretær', 'læge', 'præst', 'brandmand', 'frisør', 'politimand', 'prostitueret', 'pilot', 'soldat']#random.sample(profession_words, 10)
-    gender_specific_sample = ['konge', 'dronning', 'mor', 'far', 'han', 'hun', 'livmoder', 'penis', 'skuespiller', 'skuespillerinde']#random.sample(gender_specific_words, 10)
+    gender_specific_sample = ['konge', 'dronning', 'mor', 'far', 'han', 'hun', 'livmoder', 'penis', 'bedstefar', 'bedstemor']#random.sample(gender_specific_words, 10)
     #combined = profession_sample + gender_specific_sample
-
-    # combine wordlist as input to plot and similiarity scores 
-    #combined = career+family
-
+    '''
     # get similarity scores: professions projected onto gender direction
-    #print_similarities(args.embedding_filename, combined)
-    #print_similarities(args.debiased_filename, combined)
+    print_similarities(args.embedding_filename, combined)
+    print_similarities(args.debiased_filename, combined)
+    '''
+    # plot profession words
+    #plot_words(model, args.model_alias, profession_sample, gender_specific_sample, "Original Professions", "orig")
+    #plot_words(debiased_model, args.model_alias, profession_sample, gender_specific_sample, "Debiased Professions", "debiased")
     
-    # plot words
-    plot_words(model, args.model_alias, profession_sample, gender_specific_sample, "Original Professions", "orig")
-    plot_words(debiased_model, args.model_alias, profession_sample, gender_specific_sample, "Debiased Professions", "debiased")
-
     # plot equalize example
     eq_pairs = ['mand', 'kvinde', 'pige', 'dreng', 'bedstemor', 'bedstefar', 'mor', 'far']
     gn_word = ["skole"]
-    equalize_visualization(model, eq_pairs, gn_word, args.model_alias, "orig")
-    equalize_visualization(debiased_model, eq_pairs, gn_word, args.model_alias, "debiased")
+    equalize_visualization(model, eq_pairs, gn_word, args.model_alias, "orig", f"Gender Specific Pairs projected on 'school'")
+    equalize_visualization(debiased_model, eq_pairs, gn_word, args.model_alias, "debiased", f"Debiased Pairs projected on 'school'")
+    '''
+    #T-SNE
 
     # define wordlist for t-sne
     tsne_words = set(male+female+arts+math+family+career)
@@ -93,3 +97,4 @@ if __name__ == "__main__":
     restrict_wv(model, tsne_words)
 
     tsne_plot(model, args.model_alias)
+    '''
--- assess_bias/viz.py
+++ assess_bias/viz.py
@@ -17,8 +17,6 @@ def plot_words(embedding, model_alias, professions, gender_specific, bias_type, 
     # load x-axis
     x = np.loadtxt(os.path.join("..", "output", f"{model_alias}_gender_subspace.csv"), delimiter=',')
 
-    #x_ax = ['mand', 'kvinde']
-
     # load vector for y-axis
     y_ax = np.loadtxt(os.path.join("..", "output", f"{model_alias}_neutrality.csv"), delimiter=',')
 
@@ -30,10 +28,7 @@ def plot_words(embedding, model_alias, professions, gender_specific, bias_type, 
     
     # retrieve vectors
     vectors = [embedding[k] for k in wordlist]
-
-    # To-be basis
-    #x = (vectors[1]-vectors[0])
-    
+   
     # flipped
     y = np.flipud(y_ax)
     
@@ -67,13 +62,14 @@ def plot_words(embedding, model_alias, professions, gender_specific, bias_type, 
     plt.ylabel("Gender Neutrality")
     
     #plot the wordlist
-    plt.scatter(Wp[0,:int(len(professions))], 
+    gender_specific_scatter = plt.scatter(Wp[0,:int(len(professions))], 
         Wp[1,:int(len(professions))], 
-        color = 'orchid', marker= "o")
-    plt.scatter(Wp[0,int(len(professions)):int(len(professions+gender_specific))],
+        color = 'darkorchid', marker= "o")
+    professions_scatter = plt.scatter(Wp[0,int(len(professions)):int(len(professions+gender_specific))],
         Wp[1,int(len(professions)):int(len(professions+gender_specific))], 
         color = 'mediumseagreen', marker= "o")
     #plt.scatter(Wp[0,:2], Wp[1,:2], color = 'lightgrey', marker= "x")
+    plt.legend((gender_specific_scatter, professions_scatter),('Gender specific', 'Professions'),scatterpoints=1,loc='upper left',ncol=1,fontsize=8)
 
     rX = max(Wp[0,:])-min(Wp[0,:])
     rY = max(Wp[1,:])-min(Wp[1,:])
@@ -145,3 +141,67 @@ def tsne_plot(model, model_alias):
     plt.savefig(os.path.join("..", "output", f"tsne_plot_{model_alias}.png"))
     plt.show()
 
+
+def equalize_visualization(embedding, eq_pairs, gn_word, model_alias, biased, plot_title):
+
+    # load x-axis
+    y = np.loadtxt(os.path.join("..", "output", f"{model_alias}_gender_subspace.csv"), delimiter=',')
+
+    #combine
+    wordlist = gn_word + eq_pairs
+    
+    # choose only words that are in the embeddings
+    wordlist = [w for w in wordlist if w in embedding.vocab]
+    
+    # retrieve vectors
+    vectors = [embedding[k] for k in wordlist]
+
+  
+    x = vectors[0]
+    
+    # Get pseudo-inverse matrix
+    W = np.array(vectors)
+    B = np.array([x,y])
+    Bi = np.linalg.pinv(B.T)
+
+    # Project all the words
+    Wp = np.matmul(Bi,W.T)
+    Wp = (Wp.T-[Wp[0,2],Wp[1,0]]).T
+    
+    #PLOT
+    plt.figure(figsize=(12,7))
+    
+    plt.title(label=plot_title,
+            fontsize=30,
+            color="black")
+    #plt.xlim([-0.8, 0.8])
+    #plt.ylim([-0.25, 0.25])
+    plt.axvline(color= 'lightgrey')
+    plt.axhline(color= 'lightgrey')
+    
+    plt.xlabel("Projection of school", fontsize=22)
+    plt.ylabel("Gender subspace", fontsize=22)
+    
+    #plot the wordlist
+    plt.scatter(Wp[0,:1],Wp[1,:1], color = 'black', marker= "x", s = 200)
+    plt.scatter(Wp[0,1:3],Wp[1,1:3], color = 'mediumseagreen', marker= "o")
+    plt.plot(Wp[0,1:3],Wp[1,1:3], color = 'mediumseagreen', marker= "o", alpha =0.3, linestyle='dashed')
+    plt.scatter(Wp[0,3:5],Wp[1,3:5], color = 'darkorchid', marker= "o")
+    plt.plot(Wp[0,3:5],Wp[1,3:5], color = 'darkorchid', marker= "o", alpha =0.3, linestyle='dashed')
+    plt.scatter(Wp[0,5:7],Wp[1,5:7], color = 'dodgerblue', marker= "o")
+    plt.plot(Wp[0,5:7],Wp[1,5:7], color = 'dodgerblue', marker= "o", alpha =0.3, linestyle='dashed')
+    plt.scatter(Wp[0,7:9],Wp[1,7:9], color = 'indianred', marker= "o")
+    plt.plot(Wp[0,7:9],Wp[1,7:9], color = 'indianred', marker= "o", alpha =0.3, linestyle='dashed')
+    
+    plt.plot()
+    rX = max(Wp[0,:])-min(Wp[0,:])
+    rY = max(Wp[1,:])-min(Wp[1,:])
+    eps = 0.005
+    
+    for i, txt in enumerate(wordlist):
+        if txt == "skole":
+            plt.annotate(txt, (Wp[0,i]+rX*eps, Wp[1,i]+rY*(eps+0.02)), fontsize= 'xx-large', c = 'black')
+        else:
+            plt.annotate(txt, (Wp[0,i]+rX*eps, Wp[1,i]+rY*eps), fontsize = 'large') # changed from #plt.annotate(txt, (Wp[0,i]+rX*eps, Wp[1,i]+rX*eps))
+    
+    plt.savefig(os.path.join("..", "output", f"{biased}{model_alias}_eq_plot.png"))
